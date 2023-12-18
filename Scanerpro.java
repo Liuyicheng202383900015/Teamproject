d319d13e80cde2063dbcd3c07fe24613e6438f8f
Liuyicheng202383900015 <147969113+Liuyicheng202383900015@users.noreply.github.com>
2023-12-18 23:00:10 +0800

    Update Scanerpro.java

--- Scanerpro.java
+++ Scanerpro.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-class ScannerPro{
+import java.util.Scanner;//use scanner
+class ScannerPro{//I want to create a new way to scan
     private Scanner Scan;
 
     public String getString(){
         Scan = new Scanner(System.in);
         return Scan.nextLine();
-    }
+    } //use scan to store some
 
     public int getInt(){
         Scan = new Scanner(System.in);
@@ -13,17 +13,17 @@ class ScannerPro{
             String temp = getString();
             return Integer.parseInt(temp);
         }catch (NumberFormatException e){ return Integer.MAX_VALUE;}
-    }
+    }//use try catch to catch something wrong
 
     public int getInt(String message){
         Scan = new Scanner(System.in);
-        while (true){
+        while (true){//use while
             try {
                 String temp = getString();
                 return Integer.parseInt(temp);
             }catch (NumberFormatException ignored){
                 System.out.print(message);
-            }
+            }//another way to choose the right number to input it
         }
     }
 
@@ -33,7 +33,7 @@ class ScannerPro{
             String temp = getString();
             return Double.parseDouble(temp);
         }catch (NumberFormatException e){ return Double.MAX_VALUE;}
-    }
+    }//the way to get types of double
 
     public double getDouble(String message){
         Scan = new Scanner(System.in);
@@ -43,8 +43,7 @@ class ScannerPro{
                 return Double.parseDouble(temp);
             }catch (NumberFormatException ignored){
                 System.out.print(message);
-            }
+            }//use message to store the sentence"wrong input........
         }
     }
 }
-
